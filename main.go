8a3af6869ded43e1214d2d36941152ed85be0819
gotanda555 <crok2558@gmail.com>
2022-12-11 19:31:03 +0900

    Revert "Revert "mod: string""
    This reverts commit 980bb32db6ebf734c7c615bcec571d20df13ea3b.

--- main.go
+++ main.go
@@ -1,47 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	/*
-		var (
-			u8  uint8     = 255
-			i8  int8      = 127
-			f32 float32   = 0.2
-			c64 complex64 = -5 + 12i
-		)
-		fmt.Println(u8, i8, f32, c64)
+	fmt.Println("Hello World")
+	fmt.Println("Hello" + "World")
+	fmt.Println(string("Hello World")[0])
+	var s string = "Hello World"
 
-		fmt.Printf("type=%T value=%v", u8, u8)
-	*/
+	s = strings.Replace(s, "H", "X", 1)
+	fmt.Println(s)
+	fmt.Println(strings.Contains(s, "World"))
 
-	/*
-		x := 1 + 1
-		fmt.Println(x)
-		fmt.Println(1+1, 2+2)
-		fmt.Println("1 + 1 =", 1+1)
-		fmt.Println("10 - 1 =", 10-1)
-		fmt.Println("10 / 2 =", 10/2)
-		fmt.Println("10 / 3 =", 10/3)
-		fmt.Println("10.0 / 3 =", 10.0/3)
-		fmt.Println("10 / 3.0 =", 10/3.0)
-		fmt.Println("10 % 2 =", 10%2)
-		fmt.Println("10 % 3 =", 10%3)
-	*/
+	fmt.Println(`Test
+										Test
+Test`)
 
-	/*
-		x := 0
-		fmt.Println(x)
-		// x = x + 1
-		x++
-		fmt.Println(x)
-		// x = x - 1
-		x--
-		fmt.Println(x)
-	*/
-
-	fmt.Println(1 << 0) // 0001 0001
-	fmt.Println(1 << 1) // 0001 0010
-	fmt.Println(1 << 2) // 0001 0100
-	fmt.Println(1 << 3) // 0001 1000
+	fmt.Println("\"")
+	fmt.Println(`"`)
 }
