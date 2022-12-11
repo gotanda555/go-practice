5283c5f914f73e3cbd75753d00c59ae5f43634b7
gotanda555 <crok2558@gmail.com>
2022-12-11 21:12:26 +0900

    mod: 返り値が2つの場合

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import "fmt"
 
-func add(x, y int) int {
-	return x + y
+func add(x, y int) (int, int) {
+	return x + y, x - y
+}
+
+func cal(price, item int) (result int) {
+	result = price * item
+	return
 }
 
 func main() {
-	r := add(10, 20)
-	fmt.Println(r)
+	r1, r2 := add(10, 20)
+	fmt.Println(r1, r2)
+
+	r3 := cal(100, 2)
+	fmt.Println(r3)
 }
