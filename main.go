f621edde8372678c160e2f3071cc5a3d75b1197b
gotanda555 <crok2558@gmail.com>
2022-12-12 21:08:42 +0900

    mod: for文

--- main.go
+++ main.go
@@ -2,39 +2,28 @@ package main
 
 import "fmt"
 
-func by2(num int) string {
-	if num%2 == 0 {
-		return "ok"
-	} else {
-		return "no"
-	}
-}
-
 func main() {
-	result := by2(10)
-	if result == "ok" {
-		fmt.Println("great")
+	for i := 0; i < 10; i++ {
+		if i == 3 {
+			fmt.Println("continue")
+			continue
+		}
+
+		if i > 5 {
+			fmt.Println("break")
+			break
+		}
+		fmt.Println(i)
 	}
 
-	if result2 := by2(10); result2 == "ok" {
-		fmt.Println("great 2")
+	sum := 1
+	for sum < 10 {
+		sum += sum
+		fmt.Println(sum)
 	}
+	fmt.Println(sum)
 
-	num := 6
-	if num%2 == 0 {
-		fmt.Println("by 2")
-	} else if num%3 == 0 {
-		fmt.Println("by 3")
-	} else {
-		fmt.Println("else")
-	}
-
-	x, y := 11, 12
-	if x == 10 && y == 10 {
-		fmt.Println("&&")
-	}
-
-	if x == 10 || y == 10 {
-		fmt.Println("||")
+	for {
+		fmt.Println("hello")
 	}
 }
