db58dec4c479260d206c7884fbff70881f7ad79f
gotanda555 <crok2558@gmail.com>
2022-12-13 20:53:45 +0900

    mod: new

--- main.go
+++ main.go
@@ -4,24 +4,25 @@ import (
 	"fmt"
 )
 
-func one(x *int) {
-	*x = 1
-}
-
 func main() {
-	var n int = 100
-	one(&n)
-	fmt.Println(n)
+	s := make([]int, 0)
+	fmt.Printf("%T\n", s)
+
+	m := make(map[string]int)
+	fmt.Printf("%T\n", m)
+
+	var p *int = new(int)
+	fmt.Printf("%T\n", p)
 
 	/*
-		fmt.Println(n)
-
-		fmt.Println(&n)
-
-		var p *int = &n
-
-		fmt.Println(p)
-
+		var p *int = new(int)
 		fmt.Println(*p)
+		*p++
+		fmt.Println(*p)
+
+		var p2 *int
+		fmt.Println(p2)
+		*p2++
+		fmt.Println(p2)
 	*/
 }
