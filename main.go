1d80b383557b44ad4a5af5b14580c09f4a973356
gotanda555 <crok2558@gmail.com>
2022-12-13 21:35:26 +0900

    mod: method

--- main.go
+++ main.go
@@ -6,52 +6,24 @@ import (
 
 type Vertex struct {
 	X, Y int
-	S    string
 }
 
-func changeVertex(v Vertex) {
-	v.X = 1000
+func (v Vertex) Area() int {
+	return v.X * v.Y
 }
 
-func changeVertex2(v *Vertex) {
-	v.X = 1000
+func (v *Vertex) Scale(i int) {
+	v.X = v.X * i
+	v.Y = v.Y * i
+}
+
+func Area(v Vertex) int {
+	return v.X * v.Y
 }
 
 func main() {
-	v := Vertex{1, 2, "test"}
-	changeVertex(v)
-	fmt.Println(v)
-
-	v2 := &Vertex{1, 2, "test"}
-	changeVertex2(v2)
-	fmt.Println(*v2)
-	/*
-		v := Vertex{X: 1, Y: 2}
-		fmt.Println(v)
-		fmt.Println(v.X, v.Y)
-		v.X = 100
-		fmt.Println(v.X, v.Y)
-
-		v2 := Vertex{X: 1}
-		fmt.Println(v2)
-
-		v3 := Vertex{1, 2, "test"}
-		fmt.Println(v3)
-
-		v4 := Vertex{}
-		fmt.Printf("%T %v\n", v4, v4)
-
-		var v5 Vertex
-		fmt.Printf("%T %v\n", v5, v5)
-
-		v6 := new(Vertex)
-		fmt.Printf("%T %v\n", v6, v6)
-
-		v7 := &Vertex{}
-		fmt.Printf("%T %v\n", v7, v7)
-
-		s := make([]int, 0)
-		s := []int{}
-		fmt.Println(s)
-	*/
+	v := Vertex{3, 4}
+	// fmt.Println(Area(v))
+	v.Scale(10)
+	fmt.Println(v.Area())
 }
