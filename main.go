64ffc5454896c173e79bff6d2911b982e91df385
gotanda555 <crok2558@gmail.com>
2022-12-13 21:11:36 +0900

    mod: struct

--- main.go
+++ main.go
@@ -4,25 +4,54 @@ import (
 	"fmt"
 )
 
+type Vertex struct {
+	X, Y int
+	S    string
+}
+
+func changeVertex(v Vertex) {
+	v.X = 1000
+}
+
+func changeVertex2(v *Vertex) {
+	v.X = 1000
+}
+
 func main() {
-	s := make([]int, 0)
-	fmt.Printf("%T\n", s)
+	v := Vertex{1, 2, "test"}
+	changeVertex(v)
+	fmt.Println(v)
 
-	m := make(map[string]int)
-	fmt.Printf("%T\n", m)
-
-	var p *int = new(int)
-	fmt.Printf("%T\n", p)
-
+	v2 := &Vertex{1, 2, "test"}
+	changeVertex2(v2)
+	fmt.Println(*v2)
 	/*
-		var p *int = new(int)
-		fmt.Println(*p)
-		*p++
-		fmt.Println(*p)
+		v := Vertex{X: 1, Y: 2}
+		fmt.Println(v)
+		fmt.Println(v.X, v.Y)
+		v.X = 100
+		fmt.Println(v.X, v.Y)
 
-		var p2 *int
-		fmt.Println(p2)
-		*p2++
-		fmt.Println(p2)
+		v2 := Vertex{X: 1}
+		fmt.Println(v2)
+
+		v3 := Vertex{1, 2, "test"}
+		fmt.Println(v3)
+
+		v4 := Vertex{}
+		fmt.Printf("%T %v\n", v4, v4)
+
+		var v5 Vertex
+		fmt.Printf("%T %v\n", v5, v5)
+
+		v6 := new(Vertex)
+		fmt.Printf("%T %v\n", v6, v6)
+
+		v7 := &Vertex{}
+		fmt.Printf("%T %v\n", v7, v7)
+
+		s := make([]int, 0)
+		s := []int{}
+		fmt.Println(s)
 	*/
 }
