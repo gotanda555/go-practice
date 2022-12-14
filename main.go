9d495f3bb5717bf0dc72e0936eaf6a0941e0fcd9
gotanda555 <crok2558@gmail.com>
2022-12-14 22:40:17 +0900

    mod: custom-error

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import "fmt"
 
-type Person struct {
-	Name string
-	Age  int
+type UserNotFound struct {
+	Username string
 }
 
-func (p Person) String() string {
-	return fmt.Sprintf("My name is %v.", p.Name)
+func (e *UserNotFound) Error() string {
+	return fmt.Sprintf("User not found: %v", e.Username)
+}
+
+func myFunc() error {
+	ok := false
+	if ok {
+		return nil
+	}
+	return &UserNotFound{Username: "mike"}
 }
 
 func main() {
-	mike := Person{"Mike", 22}
-	fmt.Println(mike)
+	if err := myFunc(); err != nil {
+		fmt.Println(err)
+	}
 }
