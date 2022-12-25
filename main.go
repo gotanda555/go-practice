8b20ccbcfa5e3e00bb5012c0e19489e0433070d1
gotanda555 <crok2558@gmail.com>
2022-12-25 21:20:59 +0900

    mod: pachage

--- main.go
+++ main.go
@@ -1,25 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type UserNotFound struct {
-	Username string
-}
-
-func (e *UserNotFound) Error() string {
-	return fmt.Sprintf("User not found: %v", e.Username)
-}
-
-func myFunc() error {
-	ok := false
-	if ok {
-		return nil
-	}
-	return &UserNotFound{Username: "mike"}
-}
+	"go-practice/mylib"
+	"go-practice/mylib/under"
+)
 
 func main() {
-	if err := myFunc(); err != nil {
-		fmt.Println(err)
-	}
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Println(mylib.Average(s))
+
+	mylib.Say()
+	under.Hello()
 }
